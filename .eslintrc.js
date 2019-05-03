79a6cb7992c80593237dc4a50cb09b19aab8dfbf
Syed Farhan <syed.farhan@oliveai.com>
2019-05-02 21:27:32 -0400

    configured prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'arrow-body-style': [0, 'as-needed'],
+    'class-methods-use-this': 0,
+    'comma-dangle': [2, 'always-multiline'],
+    'import/imports-first': 0,
+    'import/newline-after-import': 0,
+    'import/no-unresolved': 2,
+    'import/prefer-default-export': 0,
+  },
+};
